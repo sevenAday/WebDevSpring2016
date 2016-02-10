c19d1683176c1190ec95d5bfb5d3eaf29caae6b2
Allen Le <ale@vestmark.com>
2016-02-09 22:46:29 -0500

    Angular exmperiment that works

--- public/assignment/js/app.js
+++ public/assignment/js/app.js
@@ -1,17 +1,4 @@
 (function(){
     angular
-        .module("FormBuilderApp", ["ngRoute"])
-        .config(function($routeProvider){
-            $routeProvider
-                .when("/", {
-                    templateUrl: "home/home.view.html",
-                    controller: "MainController"
-                })
-                .when("/profile", {
-                    templateUrl: "profile.view.html"
-                })
-                .otherwise({
-                    redirectTo: "/"
-                });
-        });
+        .module("FormBuilderApp", ["ngRoute"]);
 })();
