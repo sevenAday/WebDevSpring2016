559e27acbd8937d8f8ea41a467811bd731bab40d
Allen Le <ale@vestmark.com>
2016-02-10 22:38:05 -0500

    Include main controller.js to make this work

--- public/assignment/js/config.js
+++ public/assignment/js/config.js
@@ -2,28 +2,30 @@
 {
     angular
         .module("FormBuilderApp")
-        .config(function($routeProvider)
-        {
-            $routeProvider
-                .when("/home",
+        .config(configure);
+
+    function configure($routeProvider)
+    {
+        $routeProvider
+            .when("/home",
                 {
                     templateUrl: "/views/home/home.view.html"
                 })
-                .when("/register",
+            .when("/register",
                 {
                     templateUrl: "/views/users/register.view.html"
                 })
-                .when("/login",
+            .when("/login",
                 {
                     templateUrl: "/views/users/login.view.html"
                 })
-                .when("/profile",
+            .when("/profile",
                 {
                     templateUrl: "/views/users/profile.view.html"
                 })
-                .when("/admin",
+            .when("/admin",
                 {
                     templateUrl: "/views/users/admin.view.html"
                 })
-        });
+    }
 })();
