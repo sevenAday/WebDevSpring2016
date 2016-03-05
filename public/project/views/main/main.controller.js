591699258df9752b9dd684edd01e4a52b6fe4b6e
Allen Le <ale@vestmark.com>
2016-03-05 12:00:29 -0500

    Include commented-on for user

--- public/project/views/main/main.controller.js
+++ public/project/views/main/main.controller.js
@@ -11,14 +11,14 @@
         $rootScope.numberOfPages = 10;
         $rootScope.numberOfActivities = 10;
 
-        /*$rootScope.user = {"_id":234,
-            "firstName":"Bob",
-            "lastName":"Hope",
-            "username":"bob",
-            "password":"bob",
-            "email":"bh@bo.org",
-            "roles":["admin"]};
-        $rootScope.isAdmin = true;*/
+        $rootScope.user = {"_id": 234,
+            "firstName": "Bob",
+            "lastName": "Hope",
+            "username": "bob",
+            "password": "bob",
+            "email": "bh@bo.org",
+            "roles": ["admin"]};
+        $rootScope.isAdmin = true;
 
     }
 }());
