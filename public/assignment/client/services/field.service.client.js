8dc3d771f8c4cb4a3042db8f54f633010b208e23
Allen Le <ale@vestmark.com>
2016-03-10 00:58:18 -0500

    Complete field service client

--- public/assignment/client/services/field.service.client.js
+++ public/assignment/client/services/field.service.client.js
@@ -16,7 +16,24 @@
         };
         return service;
 
+        function createFieldForForm(formId, field) {
+            return $http.post("/api/assignment/form/" + formId + "/field", field);
+        }
 
+        function getFieldsForForm(formId) {
+            return $http.get("/api/assignment/form/" + formId + "/field");
+        }
 
+        function getFieldForForm(formId, fieldId) {
+            return $http.get("/api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+
+        function deleteFieldFromForm(formId, fieldId) {
+            return $http.delete("/api/assignment/form/" + formId + "/field/" + fieldId);
+        }
+
+        function updateField(formId, fieldId, field) {
+            return $http.delete("/api/assignment/form/" + formId + "/field/" + fieldId, field);
+        }
     }
 }());
