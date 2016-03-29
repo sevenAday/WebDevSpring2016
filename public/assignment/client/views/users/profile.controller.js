e330163dca2ab49597e24b3869ed81b16efa3d55
Allen Le <ale@vestmark.com>
2016-03-29 01:36:12 -0400

    Complete MongoDB user integration for assignment with email and phone validations

--- public/assignment/client/views/users/profile.controller.js
+++ public/assignment/client/views/users/profile.controller.js
@@ -32,7 +32,6 @@
 
         function update() {
             model.successful = false;
-            var currentUser = null;
             model.showError = true;
             if (isNotEmpty($scope.profile.username.$error)
                 || isNotEmpty($scope.profile.password.$error)
@@ -40,13 +39,31 @@
                 || isNotEmpty($scope.profile.firstName.$error)) {
                 return;
             }
-            currentUser = {
+            var emails = model.email.replace(/\s/g, "").split("|");
+            var validRegExp = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+            for (var e in emails) {
+                if (emails[e].search(validRegExp) === -1) {
+                    $scope.profile.inputEmail.$error = {"invalidEmail": true};
+                    return;
+                }
+            }
+            delete $scope.profile.inputEmail.$error.invalidEmail;
+            var phones = model.phone.replace(/\s/g, "").split("|");
+            validRegExp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+            for (var p in phones) {
+                if (phones[p].search(validRegExp) === -1) {
+                    $scope.profile.inputPhone.$error = {"invalidPhone": true};
+                    return;
+                }
+            }
+            delete $scope.profile.inputPhone.$error.invalidPhone;
+            var currentUser = {
                 "username": model.username,
                 "password": model.password,
                 "firstName": model.firstName,
                 "lastName": model.lastName,
-                "emails": model.email.replace(/\s/g, "").split("|"),
-                "phones": model.phone.replace(/\s/g, "").split("|")
+                "emails": emails,
+                "phones": phones
             };
             UserService.updateUser($rootScope.user._id, currentUser)
                 .then(function (response) {
