7c8c22dbed82ac559af1763a83d1d1014351fb0b
Allen Le <ale@vestmark.com>
2016-04-05 23:57:36 -0400

    Break up long line

--- public/project/server/app.js
+++ public/project/server/app.js
@@ -7,6 +7,7 @@ module.exports = function (passport, app, db, mongoose) {
 
     var adminService = require("./services/admin.service.server.js")(app, adminModel);
     var commentService = require("./services/comment.service.server.js")(app, commentModel);
-    var documentService = require("./services/document.service.server.js")(app, documentModel, commentModel, projectUserModel);
+    var documentService = require("./services/document.service.server.js")(app, documentModel, commentModel,
+        projectUserModel);
     var userService = require("./services/user.service.server.js")(passport, app, projectUserModel, documentModel);
 };
